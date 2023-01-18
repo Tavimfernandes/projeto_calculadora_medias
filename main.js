e5b27aa221db064545e6c9abc2767bb3c5b5823b
Octavio Fernandes <octaviofp2003@outlook.com>
2023-01-18 15:00:28 -0300

    -Fix: imagens aprovado e reprovado

--- main.js
+++ main.js
@@ -30,7 +30,7 @@ function adicionaLinha() {
         let linha = '<tr>';
         linha += `<td>${inputNomeAtividade.value}</td>`;
         linha += `<td>${inputNotaAtividade.value}</td>`;
-        linha += `<td>${inputNotaAtividade.value >= notaMinima ? 'imgAprovado' : 'imgReprovado'}</td>`;
+        linha += `<td>${inputNotaAtividade.value >= notaMinima ? imgAprovado : imgReprovado}</td>`;
         linha += '</tr>';
 
         linhas += linha;
